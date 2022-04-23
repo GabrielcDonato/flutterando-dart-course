c8c9ad4fd601be5aff9e8da3f7c37fb2e0016639
Gabriel Donato <gabrieldonato@brasilcard.net>
2022-04-23 17:26:17 -0300

    initial commit

---
+++ bin/Operations/operations.dart
@@ -0,0 +1,7 @@
+main() {
+  print(1 + 1);
+  print(1 - 4);
+  print(10 / 2);
+  print(10 * 5);
+  print(10 * 5 - (10 + 2));
+}
---
+++ bin/Variables/variable.dart
@@ -0,0 +1,5 @@
+void main() {
+  var a = 1;
+  var b = 2;
+  print(a + b);
+}

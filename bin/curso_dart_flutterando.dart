2577f31bb7926bd00029f9081757982ced8dd30f
Gabriel Donato <gabrieldonato@brasilcard.net>
2022-04-23 20:19:38 -0300

    Feat: functions

--- bin/curso_dart_flutterando.dart
+++ bin/curso_dart_flutterando.dart
@@ -1,3 +1,14 @@
 void main(List<String> arguments) {
+  c("oi");
+  division(100, 2);
   print('Hello world!');
+}
+
+c(String text) {
+  print(text);
+  print("oi2");
+}
+
+division(int a, int b) {
+  print(a / b);
 }
